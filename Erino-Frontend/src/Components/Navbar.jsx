a2f5de04c68b62215c701e2083bcdb21f0ccc059
Abhishek <abhishekkumarcse2015@gmail.com>
2024-11-16 00:27:10 +0530

    Final Commit after Updation

--- Erino-Frontend/src/Components/Navbar.jsx
+++ Erino-Frontend/src/Components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = ({ refreshContacts }) => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Contact Site
+            Contact Management
           </Typography>
           <Button
             color="inherit"
